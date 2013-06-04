58a4ddeca7eb3db8a5defe229d3b97db162acb1d
Siyuan Ma <siyuan.ma9@gmail.com>
2013-06-04 01:27:46 -0400

    fix LazyBinaryColumnarSerDe

--- tests/ssb_shot.sh
+++ tests/ssb_shot.sh
@@ -11,9 +11,9 @@ BUFS="128";	# HDFS buffer size (KB)
 #OSRS="0,128,256,512,1024";	# os read ahead buffer size (KB)
 OSRS="256";	# os read ahead buffer size (KB)
 
-REP=2;
+REP=1;
 SCALE=1;
 HDFS_DATA_PATH=/ssb_s1;
 for rg in $RGS; do
-	$WRAPD/test.sh SSB-Batch $rg ssb2 "$BUFS" "$OSRS" $REP $SCALE "$DIR/ssb2-RG${rg}" $HDFS_DATA_PATH;
+	$WRAPD/test.sh SSB-Batch $rg ssb1 "$BUFS" "$OSRS" $REP $SCALE "$DIR/ssb1-RG${rg}" $HDFS_DATA_PATH;
 done
