4be4d395d837b89c47c8c51f6d1615aef5245f63
Ubuntu <ubuntu@ip-10-139-29-171.ec2.internal>
2013-06-12 17:15:13 +0000

    reset CG_NBVAL in load.sh

--- wrapper/load.sh
+++ wrapper/load.sh
@@ -49,17 +49,17 @@ SSB_CG_NBVALS="2837046 0 624313001 17139259 166676"; #Set 0 for fixed size table
 SSB_HDFS_R=/ssb;
 SSB_HIVE_R=/user/hive/warehouse/ssb;
 
-ssb1() { base1 $TPLD/ssb.load.sql.1.template $3 ${SSB_HIVE_R}1 $1 "$SSB_BVARS" "$SSB_CG_NBVALS" $2; }
+ssb1() { base1 $TPLD/ssb.load.sql.1.template $3 ${SSB_HIVE_R}1 $1 "$SSB_BVARS" "$SSB_NBVALS" $2; }
 ssb2() { base1 $TPLD/ssb.load.sql.2.template $3 ${SSB_HIVE_R}2 $1 "$SSB_BVARS" "$SSB_CG_NBVALS" $2; }
 ssb3() { base1 $TPLD/ssb.load.sql.3.template $3 ${SSB_HIVE_R}3 $1 "$SSB_BVARS" "$SSB_CG_NBVALS" $2; }
 
 TPCH_BVARS="C_B_NUM L_B_NUM N_B_NUM O_B_NUM P_B_NUM PS_B_NUM R_B_NUM S_B_NUM";
 TPCH_NBVALS="24346144 759863287 0 171952161 24135125 118984616 0 1409184";
-TPCH_CG_NBVALS="30346144 939863287 0 171952161 24135125 118984616 0 1409184"; #869863287
+TPCH_CG_NBVALS="30346144 909863287 0 171952161 24135125 118984616 0 1409184"; #869863287
 TPCH_HDFS_R=/tpch;
 TPCH_HIVE_R=/user/hive/warehouse/tpch;
 
-tpch1() { base1 $TPLD/tpch.load.sql.1.template $3 ${TPCH_HIVE_R}1 $1 "$TPCH_BVARS" "$TPCH_CG_NBVALS" $2; }
+tpch1() { base1 $TPLD/tpch.load.sql.1.template $3 ${TPCH_HIVE_R}1 $1 "$TPCH_BVARS" "$TPCH_NBVALS" $2; }
 tpch2() { base1 $TPLD/tpch.load.sql.2.template $3 ${TPCH_HIVE_R}2 $1 "$TPCH_BVARS" "$TPCH_CG_NBVALS" $2; }
 tpch3() { base1 $TPLD/tpch.load.sql.3.template $3 ${TPCH_HIVE_R}3 $1 "$TPCH_BVARS" "$TPCH_CG_NBVALS" $2; }
 tpch_q6() { base1 $TPLD/tpch.load.sql.cg_q6.template $3 ${TPCH_HIVE_R}_q6 $1 "$TPCH_BVARS" "$TPCH_CG_NBVALS" $2; }
