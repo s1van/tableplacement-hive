c857ed8c873b2b52f50d49f343893e1f22b57973
Siyuan Ma <siyuan.ma9@gmail.com>
2013-06-15 12:44:40 -0400

    add s1000 test

--- tests/run_s100_ec2.sh
+++ tests/run_s100_ec2.sh
@@ -2,5 +2,5 @@
 CDIR="$( cd "$( dirname "$0" )" && pwd )/../";
 TESTD=$CDIR/tests;
 
-$TESTD/run_all.sh 100 3 3 $HOME/expr $HOME/store /mnt/hadoop/data $1;
-$TESTD/run_all_ssdb.sh normal 3 3 $HOME/expr $HOME/store /mnt/hadoop/data $1;
+$TESTD/run_all.sh 100 3 3 $HOME/expr/s100 $HOME/store /mnt/hadoop/data $1;
+$TESTD/run_all_ssdb.sh normal 3 3 $HOME/expr/s100 $HOME/store /mnt/hadoop/data $1;
